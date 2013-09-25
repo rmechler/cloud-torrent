ddccf625709a03fd9a2a7ed7ef57040de434521a
Roland Mechler <rmechler@gmail.com>
2013-09-25 00:00:18 -0700

    added some magnets

--- cloud-torrent.js
+++ cloud-torrent.js
@@ -1,6 +1,13 @@
 var Client = require('node-torrent');
 var client = new Client({logLevel: 'DEBUG'});
-var url = process.argv.length > 2 ? process.argv[2] : 'a.torrent';
+var torrentFile = 'a.torrent';
+//var magnet = 'magnet:?xt=urn:btih:3842cd924a0a51142fa5a6520094fdc90df7c049&dn=Chuck+Klosterman+-+Sex%2C+Drugs%2C+and+Cocoa+Puffs%3A+A+Low+Culture+Ma&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337'
+//var magnet = 'magnet:?xt=urn:btih:8e51615668c1d5a8a3e0c5dd7e245fe329cd2cf3&dn=Sex%2C+Drugs%2C+and+Cocoa+Puffs+%3A+A+Low+Culture+Manifesto+-+Chuck+Kl&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337';
+//var magnet = 'magnet:?xt=urn:btih:3842cd924a0a51142fa5a6520094fdc90df7c049&dn=Chuck+Klosterman+-+Sex%2C+Drugs%2C+and+Cocoa+Puffs%3A+A+Low+Culture+Ma&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337';
+//var magnet = 'magnet:?xt=urn:btih:3c1cc4af717dd3c7655e7888ab8363e76edfead8&dn=The+Avett+Brothers+-+Another+Is+Waiting+%28Single%29+%282013%29+%5BAAC%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337';
+var magnet = 'magnet:?xt=urn:btih:5e1d6609b8774b7b9592fd161fb5651e74f71362&dn=Kanye+West+-+Yeezus&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.istole.it%3A6969&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337';
+var torrentUrl = '';
+var url = process.argv.length > 2 ? process.argv[2] : magnet;
 var torrent = client.addTorrent(url);
 
 // when the torrent completes, move it's files to another area
